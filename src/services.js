a63d0cc0c13b1845e3f827ad99008b7e826d4a47
Hawi19 <168331546+Hawi19@users.noreply.github.com>
2025-01-29 21:49:28 +0300

    Update services.js

--- src/services.js
+++ src/services.js
@@ -1,1 +1,23 @@
+import axios from "axios";
+
+
+const axiosInstance = axios.create({
+  baseURL: "https://email-verification-backend-7vbo.onrender.com",
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    console.log(token);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
 
